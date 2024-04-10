566466e46d72fb9ddfc49134696e681da99fe7c3
Arnav Bhalgat <BHALGAAA@MAIL.UC.EDU>
2024-04-10 05:38:56 -0400

    Completing the header file

--- BeadedBag/beadedbag.h
+++ BeadedBag/beadedbag.h
@@ -6,12 +6,31 @@
 
 class BeadedBag {
 public:
-  // Place your member function declarations here.
-
+  /**
+   * Insert an Item into the Beaded Bag as long as it is not already present.
+   * @param Input the Item to be inserted
+   * @param Output nothing
+  */
+  
+  void insert (Item  to_insert);
+  
+  /**
+   * Insert an Item into the Beaded Bag as long as it is not already present.
+   * @param Input nothing
+   * @param Output the number of Items in the Beaded Bag
+  */
+  int size ();
+  
   // Here's the declaration for contains, to get you started:
   bool contains(Item maybe_contained_item);
 
+/**
+ * The vector that stores the items in the Beaded Bag.
+ * The internals of the type are hidden from the user and the user doesn't care what happens behind the curtain!
+ * 
+*/
 private:
+  std::vector<Item> bag;
 };
 
 #endif
--- BeadedBag/item.h
+++ BeadedBag/item.h
@@ -9,6 +9,14 @@ public:
   std::string getName();
   bool isEqual(Item other);
 
+/**
+ * The member variable m_name is declared as private in the Item class for encapsulation purposes. 
+ * Encapsulation is a fundamental principle of object-oriented programming that aims to hide the internal implementation details of a class and provide controlled access to its members.
+ * By declaring m_name as private, it cannot be accessed directly from outside the class. 
+ * Instead, it can only be accessed through public member functions such as getName(). 
+ * This allows the class to maintain control over how the member variable is accessed and modified, ensuring data integrity and providing a consistent interface for interacting with the class.
+ * Encapsulating the member variable also provides flexibility in terms of implementation changes. If, in the future, you decide to modify the internal representation of m_name or add additional logic, you can do so without affecting the code that uses the Item class.
+*/
 private:
   std::string m_name;
 };
