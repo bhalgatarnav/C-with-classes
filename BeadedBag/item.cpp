e9a5bf949246d24eaf59109aeb0d9699741cbff9
Arnav Bhalgat <BHALGAAA@MAIL.UC.EDU>
2024-04-10 05:27:44 -0400

    Item ADT

--- BeadedBag/item.cpp
+++ BeadedBag/item.cpp
@@ -4,9 +4,12 @@
 Item::Item(const std::string &item_name): m_name(item_name) {}
 
 std::string Item::getName() {
-  return "";
+  return this->m_name;
 }
 
 bool Item::isEqual(Item other_item) {
+  if (this->m_name == other_item.m_name) {
+    return true;
+  }
   return false;
 }
