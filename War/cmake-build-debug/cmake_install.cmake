eb611097e8a513da715290c88510d822b5074ed4
Arnav Bhalgat <148126694+bhalgatarnav@users.noreply.github.com>
2024-10-31 17:37:43 -0400

    New Logic Changes.

--- War/cmake-build-debug/cmake_install.cmake
+++ War/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/bhalgatarnav/CLionProjects/War
+# Install script for directory: /Users/bhalgatarnav/Documents/Documents - Arnav’s MacBook Pro/GitHub/C-with-classes/War
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/bhalgatarnav/CLionProjects/War/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/bhalgatarnav/Documents/Documents - Arnav’s MacBook Pro/GitHub/C-with-classes/War/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
